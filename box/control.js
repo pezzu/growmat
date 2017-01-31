7ec00e52d838f4cd239fcc1f26456250ef78b1af
pezzu <peter.sukhenko@gmail.com>
2017-01-31 20:16:08 +0200

    rpi-gpio

--- box/control.js
+++ box/control.js
@@ -1,3 +1,24 @@
+var gpio = require('rpi-gpio');
+
+// gpio.setup(7, gpio.DIR_IN, readInput);
+
+// function readInput() {
+//     gpio.read(7, function(err, value) {
+//         console.log('The value is ' + value);
+//     });
+// }
+
+// gpio.setup(7, gpio.DIR_OUT, write);
+
+// function write() {
+//     gpio.write(7, true, function(err) {
+//         if (err) throw err;
+//         console.log('Written to pin');
+//     });
+// }
+
+// https://www.npmjs.com/package/rpi-gpio
+
 module.exports.getParams = function() {
 
     return {
