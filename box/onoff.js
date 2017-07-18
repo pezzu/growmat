9894bd53798c57adb598453e4b97e920ae970b59
pezzu <peter.sukhenko@gmail.com>
2017-07-18 23:20:36 +0300

    Some corrections to POC

--- box/onoff.js
+++ box/onoff.js
@@ -8,29 +8,23 @@ var gpio = require('rpi-gpio');
 //     });
 // }
 
-gpio.setup(12, gpio.DIR_OUT, function(err) {
+gpio.setup(12, gpio.DIR_LOW, function(err) {
     if (err) throw err;
 });
 
 
 function writePy(value) {
-    console.log(value);
     gpio.write(12, value, function(err) {
         if (err) throw err;
     });
 }
 
-
-writePy(true);
-
 module.exports.turnOn = function() {
     writePy(true);
-    console.log("Turned on");
 };
 
 module.exports.turnOff = function() {
     writePy(false);
-    console.log("Turned off");
 }
 
 // https://www.npmjs.com/package/rpi-gpio
