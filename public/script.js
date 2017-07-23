5a2e6e367a52cb840345a4ad5e06c35ceb46a09f
pezzu <peter.sukhenko@gmail.com>
2017-07-24 00:27:49 +0300

    Added daylight hours selectoin

--- public/script.js
+++ public/script.js
@@ -2,29 +2,35 @@ $(document).ready(function () {
  
     function fillParams(data) {
         const controls = ['#light', '#ventilation', '#circulation']; 
-        
         controls.forEach(ctrl => $(ctrl).off('change'));
 
-        $('#temperature').append(data.temperature);
-        $('#humidity').append(data.humidity);
+        $('#temperature').text(data.temperature);
+        $('#humidity').text(data.humidity);
         $('#light').bootstrapToggle(data.light ? 'on' : 'off');
         $('#ventilation').bootstrapToggle( data.ventilation ? 'on' : 'off');
         $('#circulation').bootstrapToggle(data.circulation ? 'on' : 'off');
-        
+        $('#daylightHours').val(data.daylightHours);
+
         controls.forEach(ctrl => $(ctrl).change(onToggle));
     }
 
     function onToggle() {
         const params = {};
         params[this.id] = $(this).prop('checked') ? 1 : 0;
+        console.log(this.id + ': ' + params[this.id]);
+        $.post('api/params.json', { params: params }, fillParams);    
+    }
+
+    function onSelectDayHours() {
+        const params = {};
+        params[this.id] = $(this).val();
+        console.log(this.id + ': ' + params[this.id]);
         $.post('api/params.json', { params: params }, fillParams);    
     }
 
     // show spinner
 
+    $('#daylightHours').change(onSelectDayHours);
+
     $.getJSON('api/params.json', fillParams);
-  
-    
-  
-    
 });
