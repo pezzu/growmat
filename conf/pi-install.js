96c2439df097fafe0405656f89f62391a09c30f5
pezzu <peter.sukhenko@gmail.com>
2017-08-04 17:55:45 +0300

    Fixed PI dependencies

--- conf/pi-install.js
+++ conf/pi-install.js
@@ -4,15 +4,9 @@ if(isPi()) {
     const package = require('../package.json');
     const child_process = require('child_process');
 
-    try {
-        Object.keys(package.piDependencies).forEach(function(dependency) {
-            const command = "npm install " + dependency+"@"+package.piDependencies[dependency] + " --no-save";
-            console.log(command);
-            child_process.execSync(command);
-        });
-    }
-    catch(err) {
-        process.exit(1);
-    }
+    Object.keys(package.piDependencies).forEach(function(dependency) {
+        console.log('npm install ' + dependency+'@'+package.piDependencies[dependency] + ' --no-save');
+        child_process.spawnSync('npm', ['install', dependency+'@'+package.piDependencies[dependency], '--no-save'], {stdio: 'inherit', shell: true});
+    });
 }
 
