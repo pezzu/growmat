a5d24f4d7f91d0c28618e4f9a90c1664490beec1
pesu <peter.sukhenko@gmail.com>
2017-08-30 21:29:51 +0300

    Revert "Switched to HTTPS"
    This reverts commit 65519ea190d675d4ca4051f466d94ae9c5bebca7.

--- growmat.js
+++ growmat.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-const https = require('https');
-const fs = require('fs');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
@@ -9,14 +7,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use(express.static('public'));
 
+
 require('./app/routes.js')(app);
 
 const port = process.argv[2] || process.env.PORT || 8080;
 
-const server = https.createServer({
-    key: fs.readFileSync('cert/key.pem'),
-    cert: fs.readFileSync('cert/cert.pem')
-}, app);
-
-server.listen(port);
+app.listen(port);
 console.log("Server running at port " + port);
