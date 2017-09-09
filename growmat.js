e34264a2d3beae3a530e35ea48b20db9cde8d526
pezzu <peter.sukhenko@gmail.com>
2017-09-10 02:20:13 +0300

    Proposed new structure

--- growmat.js
+++ growmat.js
@@ -1,9 +1,7 @@
 const audit = require('./app/audit.js');
 const express = require('express');
 const app = express();
-const https = require('https');
-const fs = require('fs');
-const WebSocket = require('ws');
+const http = require('http');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
@@ -15,13 +13,7 @@ require('./app/routes.js')(app);
 
 const port = process.argv[2] || process.env.PORT || 8080;
 
-const server = https.createServer({
-    key: fs.readFileSync('cert/key.pem'),
-    cert: fs.readFileSync('cert/cert.pem')
-}, app).listen(port);
+const server = http.createServer(app);
 
-const wss = new WebSocket.Server({'server': server, perMessageDeflate: false});
-require('./video/socket.js')(wss);
-
-// server.listen(port);
+server.listen(port);
 audit.log("Server running at port " + port);
