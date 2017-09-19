968ebfeec795fafb9806d146c1cc795dc0eed3e1
pezzu <peter.sukhenko@gmail.com>
2017-09-19 23:14:49 +0300

    Video format adjusted

--- services/live-stream/ffmpeg.sh
+++ services/live-stream/ffmpeg.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z "$STREAM_PORT" ]; then
-    echo "\$STREAM_PORT is not defined!" 
+    echo "\$STREAM_PORT is not defined!"
     exit 1
-fi 
+fi
 
-ffmpeg -loglevel error -f v4l2 -r 25 -i /dev/video0 -f mpegts -codec:v mpeg1video -b:v 1000k -bf 0 http://localhost:$STREAM_PORT/supersecret
+ffmpeg -loglevel error -f v4l2 -r 25 -i /dev/video0 -f mpegts -s 1024x576 -codec:v mpeg1video -b:v 1000k -bf 0 http://localhost:$STREAM_PORT/supersecret
